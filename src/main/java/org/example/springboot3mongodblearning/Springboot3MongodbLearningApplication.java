e1c51acc733ad12592550f3fdeb04fecfa637869
Luscaee <leleodias19@gmail.com>
2025-07-22 17:58:45 -0300

    first commit

---
+++ src/main/java/org/example/springboot3mongodblearning/Springboot3MongodbLearningApplication.java
@@ -0,0 +1,13 @@
+package org.example.springboot3mongodblearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3MongodbLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3MongodbLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springboot3mongodblearning/Springboot3MongodbLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboot3mongodblearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3MongodbLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
